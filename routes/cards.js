cd8548276d764e0c776a6dbab252b54470e2ad91
sneg <sneg@example.com>
2016-11-02 22:51:46 +0200

    update code style via eslint

--- routes/cards.js
+++ routes/cards.js
@@ -1,43 +1,43 @@
 const express = require('express');
+
 const router = express.Router();
 const db = require('../db.json');
-
-let lastId = getLastId(db);
 
 function getIndex(arr, id) {
   return arr.findIndex(item => item.id === Number(id));
 }
 
-function getLastId(db) {
-  return Math.max(...db.cards.map(item => item.id));
-}
-
-function getRandomBackgroundColor() {
-  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+function getLastId(dataBase) {
+  return Math.max(...dataBase.cards.map(item => item.id));
 }
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-router.get('/', function (req, res, next) {
+function getRandomBackgroundColor() {
+  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
+
+const lastId = getLastId(db);
+
+router.get('/', (req, res) => {
   res.status(200);
   res.json(db.cards);
 });
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', (req, res) => {
   const index = getIndex(db.cards, req.params.id);
   if (index === -1) {
     res.status(404).send('404');
-  }
-  else {
+  } else {
     res.status(200);
     res.json(db.cards[index]);
   }
 });
 
-router.post('/', function (req, res, next) {
-  req.body.id = ++lastId;
+router.post('/', (req, res) => {
+  req.body.id = lastId + 1;
   req.body.title += ` ${lastId}`;
   req.body.background = getRandomBackgroundColor();
 
@@ -45,13 +45,13 @@ router.post('/', function (req, res, next) {
   res.json(db.cards);
 });
 
-router.put('/:id', function (req, res, next) {
+router.put('/:id', (req, res) => {
   const index = getIndex(db.cards, req.params.id);
   Object.assign(db.cards[index], req.body);
   res.json(db.cards[index]);
 });
 
-router.delete('/:id', function (req, res, next) {
+router.delete('/:id', (req, res) => {
   const index = getIndex(db.cards, req.params.id);
   db.cards.splice(index, 1);
   res.json(db.cards);
